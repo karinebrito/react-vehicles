c82f180f16e035354c7aaf4168a6c0ca4be7b08d
karinebrito <kariine.brito@hotmail.com>
2022-07-03 16:10:20 -0300

    added list from localstorage

--- src/pages/CarList/index.js
+++ src/pages/CarList/index.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function CarList() {
+  const [cars, setCars] = useState(() => {
+    const carsList = localStorage.getItem("cars");
+
+    if (carsList) {
+      return JSON.parse(carsList);
+    } else {
+      return [];
+    }
+  });
+
   return (
     <>
       <h1 className="container-title">Veículos Cadastrados</h1>
@@ -22,35 +32,31 @@ export default function CarList() {
       </div>
 
       <table className="carlist-table">
-        <tr>
-          <th>Chassi</th>
-          <th>Renavam</th>
-          <th>UF</th>
-          <th>Placa</th>
-          <th>Ano de Fabricação</th>
-          <th>Ano de Modelo</th>
-          <th>Cor</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Chassi</th>
+            <th>Renavam</th>
+            <th>UF</th>
+            <th>Placa</th>
+            <th>Ano de Fabricação</th>
+            <th>Ano de Modelo</th>
+            <th>Cor</th>
+          </tr>
+        </thead>
 
-        <tr>
-          <td>123abc</td>
-          <td>1234</td>
-          <td>teste</td>
-          <td>1234</td>
-          <td>1234</td>
-          <td>1234</td>
-          <td>teste</td>
-        </tr>
-
-        <tr>
-          <td>123abc</td>
-          <td>1234</td>
-          <td>teste</td>
-          <td>1234</td>
-          <td>1234</td>
-          <td>1234</td>
-          <td>teste</td>
-        </tr>
+        <tbody>
+          {cars.map((car) => (
+            <tr key={car.id}>
+              <td>{car.chassi}</td>
+              <td>{car.renavam}</td>
+              <td>{car.UF}</td>
+              <td>{car.placa}</td>
+              <td>{car.productionYear}</td>
+              <td>{car.modelYear}</td>
+              <td>{car.color}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
